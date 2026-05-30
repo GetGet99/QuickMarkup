fbc63e5545606b9d414a9bc84ef70e06588aae8e
Get <getdna99@gmail.com>
2026-05-30 13:16:24 +0700

    fix: compilation error

--- QuickMarkup.CodeAnalysis/QuickMarkupAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace QuickMarkup.SourceGen;
-
-#pragma warning disable CS9113 // Parameter is unread.
-class QuickMarkupAttribute(string markup) : Attribute;
-#pragma warning restore CS9113 // Parameter is unread.
