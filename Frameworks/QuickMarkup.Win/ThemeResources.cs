9342a12418dbdc9b1c299e9a69c245768bbd5040
Get <getdna99@gmail.com>
2026-06-13 13:13:37 +0700

    fix: resource on non-UI thread

--- Frameworks/QuickMarkup.Win/ThemeResources.cs
+++ Frameworks/QuickMarkup.Win/ThemeResources.cs
@@ -1,15 +1,46 @@
 using Windows.UI.ViewManagement;
+#if UWP
+using Windows.UI.Core;
+using Windows.ApplicationModel.Core;
+#else
+using Microsoft.UI.Dispatching;
+#endif
 
 namespace QuickMarkup.WinUI;
 
 public partial class ThemeResources
 {
     static UISettings UISettings { get; } = new();
+
+#if UWP
+    static CoreDispatcher? _dispatcher;
+#else
+    static DispatcherQueue? _dispatcher;
+#endif
+
+    static void EnsureDispatcher()
+    {
+#if UWP
+        _dispatcher ??= CoreApplication.GetCurrentView().Dispatcher;
+#else
+        _dispatcher ??= DispatcherQueue.GetForCurrentThread();
+#endif
+    }
+
+    static void RunOnUIThread(Action action)
+    {
+#if UWP
+        _ = _dispatcher!.TryRunAsync(CoreDispatcherPriority.High, () => action());
+#else
+        _ = _dispatcher!.TryEnqueue(() => action());
+#endif
+    }
+
     public static Reference<T?> Get<T>(string resourcesName, FrameworkElement? element)
     {
         if (element is null)
             return Get<T>(resourcesName);
-        
+
         var prop = new Reference<T?>(Resolve<T>(resourcesName, element));
         element.ActualThemeChanged += delegate
         {
@@ -17,14 +48,19 @@ public partial class ThemeResources
         };
         return prop;
     }
+
     public static Reference<T?> Get<T>(string resourcesName)
     {
         bool isDark = UISettings.GetColorValue(UIColorType.Background).R < 255 / 2;
         var prop = new Reference<T?>(Resolve<T>(resourcesName, isDark));
+        EnsureDispatcher();
         UISettings.ColorValuesChanged += delegate
         {
-            bool isDark = UISettings.GetColorValue(UIColorType.Background).R < 255 / 2;
-            prop.Value = Resolve<T>(resourcesName, isDark);
+            RunOnUIThread(() =>
+            {
+                bool isDark = UISettings.GetColorValue(UIColorType.Background).R < 255 / 2;
+                prop.Value = Resolve<T>(resourcesName, isDark);
+            });
         };
         return prop;
     }
