6f93bc2e5a5ff1d83d1bd6a4c724a8863a096197
Get <getdna99@gmail.com>
2026-07-05 16:11:44 +0700

    usethemebrushes should be public

--- Frameworks/QuickMarkup.Win/ThemeResourcesExtension.cs
+++ Frameworks/QuickMarkup.Win/ThemeResourcesExtension.cs
@@ -2,7 +2,7 @@ using System.Runtime.CompilerServices;
 
 namespace QuickMarkup.WinUI;
 
-static class ThemeResourcesExtension
+public static class ThemeResourcesExtension
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ThemeBrushes UseThemeBrushes(this FrameworkElement element) => new(element);
