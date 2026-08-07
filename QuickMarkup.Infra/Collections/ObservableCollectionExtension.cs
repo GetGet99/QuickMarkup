c175978212b37513bd4ebe61c1fef6aabaf44fc6
Get <getdna99@gmail.com>
2026-08-08 03:02:30 +0700

    line breaks

--- QuickMarkup.Infra/Collections/ObservableCollectionExtension.cs
+++ QuickMarkup.Infra/Collections/ObservableCollectionExtension.cs
@@ -8,11 +8,14 @@ public static class ObservableCollectionExtension
     extension<T>(ObservableCollection<T> collection)
     {
         /// <summary>
-        /// Wraps <see cref="ObservableCollection{T}"/> into an <see cref="IReference{T}"/>. It notifies whenever <see cref="ObservableCollection{T}"/> collection changed.
+        /// Wraps <see cref="ObservableCollection{T}"/> into an <see cref="IReference{T}"/>.
+        /// It notifies whenever <see cref="ObservableCollection{T}"/> collection changed.
         /// </summary>
         /// <remarks>
-        /// Reference always return the same <see cref="ObservableCollection{T}"/> instance <c>ReferenceEquals(myCollection, myCollection.ReactiveProp.Value)</c>.
-        /// However, <c>myCollection.ReactiveProp.Value</c> will participate in QuickMarkup reactive chain. <c>.Value</c> getter must be invoked in reactive tracking context for the reference to be tracked.<br/>
+        /// Reference always return the same <see cref="ObservableCollection{T}"/> instance
+        /// <c>ReferenceEquals(myCollection, myCollection.ReactiveProp.Value)</c>.
+        /// However, <c>myCollection.ReactiveProp.Value</c> will participate in QuickMarkup reactive chain.
+        /// <c>.Value</c> getter must be invoked in reactive tracking context for the reference to be tracked.<br/>
         /// <code>
         /// var prop = myCollection.ReactiveProp;
         /// var sample1 = Computed(() => ReactiveProp.Value.Count); // this is reactive
@@ -35,8 +38,10 @@ public static class ObservableCollectionExtension
         /// Enable <see cref="ObservableCollection{T}"/> to participate in reactive chain.
         /// </summary>
         /// <remarks>
-        /// Property returns the same <see cref="ObservableCollection{T}"/> instance <c>ReferenceEquals(myCollection, myCollection.Reactive)</c>.
-        /// However, <c>myCollection.Reactive</c> will participate in QuickMarkup reactive chain. <c>.Reactive</c> getter must be invoked in reactive tracking context for the reference to be tracked.<br/>
+        /// Property returns the same <see cref="ObservableCollection{T}"/> instance
+        /// <c>ReferenceEquals(myCollection, myCollection.Reactive)</c>.
+        /// However, <c>myCollection.Reactive</c> will participate in QuickMarkup reactive chain.
+        /// <c>.Reactive</c> getter must be invoked in reactive tracking context for the reference to be tracked.<br/>
         /// <code>
         /// var sample1 = Computed(() => myCollection.Reactive.Count); // this is reactive
         /// var collection2 = myCollection.Reactive;
